0a4cd9801ca754f755748ea477f4832c93d28a1e
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-11-22 02:38:38 -0300

    first commit

---
+++ src/main/java/com/example/tpjavahomebanking/TpJavaHomebankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpjavahomebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJavaHomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpJavaHomebankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tpjavahomebanking/TpJavaHomebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpjavahomebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJavaHomebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
