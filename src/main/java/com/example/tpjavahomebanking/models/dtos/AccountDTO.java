24ea79dd7a58c01699f094dfee482a5a71b026ed
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-05 12:51:17 -0300

    Added AccountDTO

---
+++ src/main/java/com/example/tpjavahomebanking/models/dtos/AccountDTO.java
@@ -0,0 +1,19 @@
+package com.example.tpjavahomebanking.models.dtos;
+
+import com.example.tpjavahomebanking.models.enums.AccountType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AccountDTO {
+    private Long id;
+    private AccountType type;
+    private String cbu;
+    private String alias;
+    private BigDecimal amount;
+}
