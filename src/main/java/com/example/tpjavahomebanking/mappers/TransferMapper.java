ba7eeeca1cfb97329f5d505ed69ff200483cb954
CANDELA ABREGU <candela.abregu1@gmail.com>
2023-12-09 15:23:41 -0300

    Add transferMapper

--- src/main/java/com/example/tpjavahomebanking/mappers/TransferMapper.java
+++ src/main/java/com/example/tpjavahomebanking/mappers/TransferMapper.java
@@ -1,2 +1,29 @@
-package com.example.tpjavahomebanking.mappers;public class TransferMapper {
+package com.example.tpjavahomebanking.mappers;
+
+
+import com.example.tpjavahomebanking.models.entity.Transfer;
+import com.example.tpjavahomebanking.models.dtos.TransferDTO;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TransferMapper {
+
+    public Transfer dtoToTransfer(TransferDTO dto){
+        return Transfer.builder()
+                .amount(dto.getAmount())
+                .date(dto.getDate())
+                .origin(dto.getOrigin())
+                .target(dto.getTarget())
+                .build();
+    }
+
+    public TransferDTO transferToDto(Transfer transfer){
+        return TransferDTO.builder()
+                .id(transfer.getId())
+                .amount(transfer.getAmount())
+                .target(transfer.getTarget())
+                .origin(transfer.getOrigin())
+                .date(transfer.getDate())
+                .build();
+    }
 }
