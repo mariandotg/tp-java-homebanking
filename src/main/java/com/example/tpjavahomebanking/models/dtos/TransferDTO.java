1150887e5a37711980f2c3294755936717fe8305
CANDELA ABREGU <candela.abregu1@gmail.com>
2023-12-09 15:01:52 -0300

    add tranfer-dto

--- src/main/java/com/example/tpjavahomebanking/models/dtos/TransferDTO.java
+++ src/main/java/com/example/tpjavahomebanking/models/dtos/TransferDTO.java
@@ -1,2 +1,23 @@
-package com.example.tpjavahomebanking.models.dtos;public class TransferDTO {
+package com.example.tpjavahomebanking.models.dtos;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+
+
+public class TransferDTO {
+    private Long id;
+    private Long origin;
+    private Long target;
+    private Long date;
+
+    private BigDecimal amount;
+
 }
+
